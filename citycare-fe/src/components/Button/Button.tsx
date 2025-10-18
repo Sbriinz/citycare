6c324a1346f09deadf24c57a6fe66f4ab8bce8ae
Sbriinz <stefanoferralis@gmail.com>
2025-10-18 13:47:40 +0200

    Code refactor and implemented report map

--- citycare-fe/src/components/Button/Button.tsx
+++ citycare-fe/src/components/Button/Button.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Button.css";
-import "./pages/Home"
+import "../../pages/Home"
 import {useNavigate} from "react-router-dom";
 
 
--- citycare-fe/src/pages/ReportsMapPage.tsx
+++ citycare-fe/src/pages/ReportsMapPage.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { MapContainer, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import "./ReportsMapPage.css";
 
 const ReportsMapPage: React.FC = () => {
-    return <h1>Pagina Mappa Segnalazioni</h1>;
+    return (
+        <MapContainer
+            // @ts-ignore
+            center={[40.29709305551441, 8.498636830411488]}
+            zoom={17}
+            style={{ height: "100vh", width: "100%" }}
+        >
+            <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                // @ts-ignore
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            />
+        </MapContainer>
+    );
 };
 
 export default ReportsMapPage;
